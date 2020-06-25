36b52c84f2899452fce431dd7e6cce27845cd310
sonsoleslp <poynting.slp@gmail.com>
2020-06-25 13:50:54 +0200

    Update

--- README.md
+++ README.md
@@ -1,25 +1,248 @@
-# Comprobador Automático mooc_git-entrega1_merge
 
-Puede descargar este proyecto en el ordenador local de dos formas:
-1. Clonar el  comprobador con el comando `git clone ..`
-2. Entrar al  comprobador con el navegador y descargar el fichero del proyecto ZIP con un botón.
+<img  align="left" width="150" style="float: left;" src="https://www.upm.es/sfs/Rectorado/Gabinete%20del%20Rector/Logos/UPM/CEI/LOGOTIPO%20leyenda%20color%20JPG%20p.png">
+<img  align="right" width="150" style="float: right;" src="https://miriadax.net/miriadax-theme/images/custom/logo_miriadax_new.svg">
 
-El proyecto se descarga, instala y ejecuta en el ordenador local con los siguientes comandos:
+<br/><br/><br/>
 
-```sh
-$ ## El proyecto también puede copiarse en el ordenador local con:
-$ git clone https://github.com/practicas-ging/mooc_git-entrega1_merge
-$
-$ cd mooc_git-entrega1_merge  ## Entrar en el directorio de trabajo del programa de test
-$
-$ npm install  ## Instala el programa de test, que es un paquete npm
-$
-$ npm run checks [nombre_de_la_cuenta]  ## Pasa los tests sobre el repositorio en github
-...................................## indicando que partes  están correctamente
-...................................## implementadas y cuales no.
-... (resultado de los tests)
-$
+# Módulo 3: Repositorios local y remoto, directorio de trabajo, commit, raw máster (principal), ramas remotas, GitHub y los comandos Git: add, checkout, diff, init, log, mv, reset, rm, status y commit - Entrega P2P: Commit
+
+## Objetivos
+ * Practicar con repositorios locales y remotos
+ * Crear ramas
+ * Integrar ramas
+ * Resolver conflictos
+
+## Descripción de la práctica
+
+En esta práctica se parte del repositorio creado en la entrega anterior, realizando modificaciones sobre el mismo para comenzar a trabajar con ramas de git. Crearemos una rama nueva con un desarrollo paralelo y la integraremos con la rama master resolviendo cualquier conflicto que haya podido surgir.
+
+
+## Tareas
+
+**Si ha realizado la entrega anterior, puede saltar directamente al paso 7.**
+
+
+### Paso 1: Creación de repositorio remoto
+El primer paso a seguir es crear una cuenta en Github, si no se tiene. A continuación, creamos un nuevo repositorio con el nombre "my_calculator".
+
+### Paso 2: Creación de repositorio local
+En un terminal de nuestro ordenador iniciamos un repositorio local de git. Si se tiene el sistema operativo Windows, se recomienda emplear [Git Bash](https://gitforwindows.org/) como terminal.
+```
+$ git init my_calculator
+$ cd my_calculator
+```
+y le asignamos el repositorio remoto que acabamos de crear
+```
+$ git remote add origin https://github.com/<mi usuario de github>/my_calculator.git
 ```
 
-Debe cambiar **\[nombre_de_la_cuenta]** por el nombre identificador de su repositorio en GitHub, por
-ejemplo **evaperez1**.
+### Paso 3: Añadir ficheros al repositorio
+
+Añadir al directorio de trabajo un fichero con el nombre "index.html". Este fichero contendrá una calculadora web con un botón para calcular el cubo del número introducido:
+```
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Calculator</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<h1>Calculadora de ……su nombre y apellidos……</h1>
+		Number:
+		<input type="text" id="n1">
+		<p>
+			<button onclick="cube()"> x^3 </button>
+		</p>
+		<script type="text/javascript">
+			function cube() {
+				var num = document.getElementById("n1");
+				num.value = Math.pow(num.value, 3);
+			}
+		</script>
+	</body>
+</html>
+```
+En la etiqueta h1 debes modificar el texto para incluir tu nombre y apellidos
+
+### Paso 4: Registrar cambios
+A continuación, hay que registrar los ficheros en el índice y crear el primer commit en la rama master. Se recuerda que antes de crear un commit hay que probar siempre que el programa que se va a guardar funciona correctamente (en este caso, abriéndolo en el navegador web y probando que funciona la calculadora).
+
+```
+$ git add index.html # Añadir el fichero creado
+$ git commit -m "x^3 button" # Congelar los cambios en un commit
+$ git log --oneline # Ver la lista de commits
+```
+
+## Paso 5: Crear un segundo commit
+Debe crear un segundo commit en la rama en la que está trabajando (master). 
+El commit debe añadir a la calculadora (fichero index.html) un segundo botón (elemento HTML `<button ..>`) que eleve un número a la cuarta potencia (x^4) invocando una función (power_4 ()) que calcula x^4 al hacer click en él.
+
+```
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Calculator</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<h1>Calculadora de ……su nombre y apellidos……</h1>
+		Number:
+		<input type="text" id="n1">
+		<p>
+			<button onclick="cube()"> x^3 </button>
+			<button onclick="power_4()"> x^4</button>
+		</p>
+		<script type="text/javascript">
+			function cube() {
+				var num = document.getElementById("n1");
+				num.value = Math.pow(num.value, 3);
+			}
+			function power_4() {
+				var num = document.getElementById("n1");
+				num.value = Math.pow(num.value, 4);
+			}
+		</script>
+	</body>
+</html>
+```
+
+Una vez añadido el código del nuevo nuevo botón a la calculadora y después de probar que funciona correctamente, registrar los cambios en el índice y crear el nuevo commit.
+
+```
+$ git add index.html
+$ git commit -m "x^4 button"
+```
+
+### Paso 6: Subir el repositorio a Github
+
+Para finalizar, hay que subir el repositorio local al repositorio remoto creado en Github inicialmente.
+
+```
+$ git push origin master
+```
+
+### Paso 7: Crear nueva rama
+
+Imaginemos que un miembro de nuestro equipo comenzase el desarrollo de una nueva funcionalidad partiendo de la versión inicial (sin el botón x^4). Para ello, creamos una rama de nombre "sine" que salga del primer commit (con mensaje "x^3 button") y nos movemos a la rama "sine" en el directorio de trabajo, para poder trabajar sobre ella. Primero tenemos que averiguar cual es el id del commit de nombre "x^3 button" y después crear la rama nueva a partir de él. 
+
+```
+$ git log --oneline                  # Ver la lista de commits (incluye <commit_id>, p.e. 71e69ce)
+$ git checkout -b sine <commit_id>   # Crea rama en commit y realiza checkout a rama
+```
+
+### Paso 8: Crear nuevo commit 
+
+
+Crear un nuevo commit en la rama sine que añada un segundo botón sin(x) a la calculadora en el fichero index.html (además del botón x^3 que ya existe). El nuevo botón debe calcular el seno de un número utilizando la función JavaScript:  Math.sin(x).
+
+```
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Calculator</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<h1>Calculadora de ……su nombre y apellidos……</h1>
+		Number:
+		<input type="text" id="n1">
+		<p>
+			<button onclick="cube()"> x^3 </button>
+			<button onclick="sine()"> sin </button>
+		</p>
+		<script type="text/javascript">
+			function cube() {
+				var num = document.getElementById("n1");
+				num.value = Math.pow(num.value, 3);
+			}
+			function sine() {
+				var num = document.getElementById("n1");
+				num.value = Math.sin(num.value);
+			}
+		</script>
+	</body>
+</html>
+```
+
+Estando en la rama sine, se añade el código del nuevo nuevo botón a la calculadora y una vez comprobado que funciona correctamente, registrar los cambios en el índice y crear el nuevo commit con el nombre "sin(x) button".
+
+```
+$ git add index.html
+$ git commit -m "sin(x) button"
+```
+
+
+### Paso 9: Integrar la rama "master" en "sine"
+
+Ahora se debe integrar la rama "master" en la rama "sine", para crear una calculadora con tres botones: x^3, x^4 y sin(x).
+
+```
+$ git merge master
+```
+
+La integración tiene conflictos, que se deben resolver con el editor. Una vez resueltos y después de comprobar que el programa integrado funciona correctamente, se debe finalizar la integración (merge) creando el commit de integración.
+
+```
+$ git add index.html
+$ git merge --continue # o git commit -m "Integrate master"
+```
+
+### Paso 10: Integrar la rama "sine" en "master"
+
+
+Ahora que la rama "sine" está correcta y contiene todas las funcionalidades, podemos integrar los cambios en la rama principal "master",
+Para ello, nos cambiamos a la rama "master" y nos traemos los cambios de "sine".
+
+```
+git merge sine
+```
+
+Si queremos podemos ver la historia de integraciones de todas las ramas:
+```
+git log --oneline --graph --all
+```
+
+### Paso 11: Subir todas las ramas del repositorio local al nuevo repositorio en GitHub.
+
+Por último, subimos los cambios realizados en ambas ramas a Github
+
+```
+git push --all
+```
+
+La opción --force o -f permite subir un repositorio incompatible, pero ¡Cuidado borra el existente! Se debe utilizar solo en casos en que no hay otra solución.
+
+
+Si accedemos al repositorio creado en Github podemos ver que están ambas ramas con el contenido que hemos subido.
+
+## Prueba de la práctica 
+
+Para ayudar al desarrollo, se provee una herramienta de autocorrección que prueba las distintas funcionalidades que se piden en el enunciado. Para utilizar esta herramienta debes tener node.js (y npm) ([https://nodejs.org/es/](https://nodejs.org/es/)) y Git instalados. Primero ejecute los siguientes comandos en un directorio diferente al de la práctica:
+
+```
+$ git clone https://github.com/ging-moocs/MOOC_quiz_mod4-merge_entrega
+$ cd MOOC_quiz_mod4-merge_entrega
+```
+
+A continuación guarde en un fichero llamado 'git_account' su nombre de usuario de GitHub
+```
+echo "mi_nombre_de_usuario_en_github" >> git_account
+```
+
+Para instalar y hacer uso de la [herramienta de autocorrección](https://www.npmjs.com/package/moocauto) en el ordenador local, ejecuta los siguientes comandos
+
+```
+$ npm install -g moocauto     ## Instala el programa de test
+$ moocauto                    ## Pasa los tests al fichero a entregar
+............................  ## en el directorio de trabajo
+... (resultado de los tests)
+```
+También se puede instalar como paquete local, en el caso de que no se dispongas de permisos en el ordenador desde el que estás trabajando:
+```
+$ npm install moocauto         ## Instala el programa de test
+$ npx moocauto                 ## Pasa los tests al fichero a entregar
+............................   ## en el directorio de trabajo
+... (resultado de los tests)
+```
+
+Se puede pasar la herramienta de autocorrección tantas veces como se desee.
--- tests/checks.js
+++ tests/checks.js
@@ -4,7 +4,6 @@
 
 
 // IMPORTS
-const should = require('chai').should();
 const git = require('simple-git/promise');
 const Utils = require("./utils");
 const to = require("./to");
@@ -12,20 +11,12 @@ const path = require('path');
 const fs = require('fs-extra');
 
 
-// Parse input arguments
-const args = JSON.parse(JSON.stringify(process.argv));
-// Gets the student name from args[2]
-if (!(args.length > 2)) {
-    console.error("GitHub repository name not found");
-    process.exit(1);
-}
-const student = args[2];
+
 
 // CONSTS
 const REPO_NAME = 'my_calculator';
 const BRANCH_NAME = 'remotes/origin/sine';
 const PATH_ASSIGNMENT = path.resolve(path.join(__dirname, "../"));
-const REPO_URL = `git://github.com/${student}/${REPO_NAME}.git`;
 const PATH_REPO = path.join(PATH_ASSIGNMENT, REPO_NAME);
 
 
@@ -36,20 +27,31 @@ let branches = null;
 let commit_1_master = null;
 let commit_2_master = null;
 let commit_head_sine = null;
+let student = null;
+let REPO_URL = "";
 
 
 let mygit = git(PATH_ASSIGNMENT);
 
-describe('mooc_git-entrega1_merge', function () {
+describe('Merge', function () {
+    it("(Prechecks) Comprobando", async function () {
+        this.score = 0;
+        this.msg_err = "No se ha encontrado el fichero 'git_account' que debe contener el nombre de usuario de github";
 
-    it("", async function () {
-        this.name = "1. Looking for the master branch";
-        this.score = 1;
-        this.msg_ok = `Master branch found at ${REPO_URL}`;
+        student = fs.readFileSync(path.join(PATH_ASSIGNMENT, 'git_account'), {encoding: 'utf8'}).replace(/^\s+|\s+$/g, '');;
+        console.log(student)
+        REPO_URL = `git://github.com/${student}/${REPO_NAME}.git`;
+        this.msg_ok = `Se ha encontrado el fichero 'git_account': ${student}`;
+        should.exist(student);
+    });
+
+    it("(Prechecks) Buscando la rama master", async function () {
+        this.score = 0.25;
+        this.msg_ok = `Se ha encontrado la rama master ${REPO_URL}`;
         [_, _] = await to(fs.remove(PATH_REPO));
         [error_repo, _] = await to(mygit.clone(REPO_URL));
         if (error_repo) {
-            this.msg_err = `Master branch not found at ${REPO_URL}.\n\t\tError: >>${error_repo}<<`;
+            this.msg_err = `No se encuentra rama master en ${REPO_URL}`;
             error_critical = this.msg_err;
             should.not.exist(error_critical);
         }
@@ -57,73 +59,70 @@ describe('mooc_git-entrega1_merge', function () {
         should.not.exist(error_repo);
     });
 
-    it("", async function () {
-        const expected = 'README.md';
-        this.name = `2. Looking for the file '${expected}' in the master branch first commit`;
-        this.score = 1;
+    it("Buscando commits en la rama master", async function () {
+        this.score = 0.25;
         if (error_critical) {
             this.msg_err = error_critical;
             should.not.exist(error_critical);
         } else {
             [error_log, log] = await to(mygit.log());
             if (error_log) {
-                this.msg_err = `Error reading logs from ${PATH_REPO}`;
+                this.msg_err = `Error al leer los logs en ${PATH_REPO}`;
                 error_critical = this.msg_err;
                 should.not.exist(error_critical);
             }
             if (log.all.length < 2){
-                this.msg_err = `Expected at least 2 commits in ${REPO_URL}, found: ${log.all.length}`;
+                this.msg_err = `Se esperaban por lo menos 2 commits en ${REPO_URL}. Resultado: ${log.all.length}`;
                 error_critical = this.msg_err;
                 should.not.exist(error_critical);
             }
             commit_1_master = log.all[log.all.length - 1].hash.substring(0, 7);
             commit_2_master = log.all[log.all.length - 2].hash.substring(0, 7);
             if (!commit_1_master) {
-                this.msg_err = `Error: master branch commit not found.\n\t\t\tReceived: ${log.all[log.all.length - 1]}`;
+                this.msg_err = `Error: al leer el commit en la rama master.\n\t\t\tResultado: ${log.all[log.all.length - 1]}`;
                 error_critical = this.msg_err;
                 should.not.exist(error_critical);
 
             }
             [error_commit, output] = await to(mygit.show([commit_1_master, '--name-only', '--pretty=format:']));
             if (error_commit) {
-                this.msg_err = `Error reading the master branch commit ${commit_1_master}`;
+                this.msg_err = `Error al leer el commit ${commit_1_master} de la rama master`;
                 error_critical = this.msg_err;
                 should.not.exist(error_critical);
             }
-            this.msg_ok = `Found the file '${expected}' in the master branch commit ${commit_1_master}`;
-            this.msg_err = `File '${expected}' not found in the master branch commit ${commit_1_master}`;
-            Utils.search(expected, output).should.be.equal(true);
+            this.msg_ok = `Se ha encontrado el primer commit en la rama master ${commit_1_master}`;
+            this.msg_err = `No se encuentra el primer commit de la rama master ${commit_1_master}`;
         }
     });
 
-    it("", async function () {
-        const expected = "calculator.html";
-        this.name = `3. Looking for the file '${expected}' in the master branch first commit`;
-        this.score = 1;
+    it("Buscando el fichero index.html en el primer commit", async function () {
+        const expected = "index.html";
+        this.score = 0.25;
         if (error_critical) {
             this.msg_err = error_critical;
             should.not.exist(error_critical);
         } else {
-            this.msg_ok = `Found the file '${expected}' in the master branch commit ${commit_1_master}`;
-            this.msg_err = `File '${expected}' not found in the master branch commit ${commit_1_master}`;
+            this.msg_ok = `Se ha encontrado el fichero '${expected}'  en el primer commit del la rama master ${commit_1_master}`;
+            this.msg_err = `El fichero '${expected}' no se encuentra en el primer commit de la rama master ${commit_1_master}`;
+
             Utils.search(expected, output).should.be.equal(true);
         }
     });
 
-    it("", async function () {
-        const expected = "calculator.html";
-        this.name = `4. Looking for the file '${expected}' in the master branch second commit`;
-        this.score = 1;
+    it("Buscando el fichero index.html en el segundo commit", async function () {
+        const expected = "index.html";
+        this.score = 0.25;
         if (error_critical) {
             this.msg_err = error_critical;
             should.not.exist(error_critical);
         } else {
             let output;
-            this.msg_ok = `Found the file '${expected}' in the master branch commit ${commit_2_master}`;
-            this.msg_err = `File '${expected}' not found in the master branch commit ${commit_2_master}`;
+            this.msg_ok = `Se ha encontrado el fichero '${expected}'  en el primer commit del la rama master ${commit_2_master}`;
+            this.msg_err = `El fichero '${expected}' no se encuentra en el primer commit de la rama master ${commit_2_master}`;
+
             [error_show, output] = await to(mygit.show([commit_2_master]));
             if (error_show){
-                this.msg_err = `Error reading the master branch commit ${commit_2_master}`;
+                this.msg_err = `Error al leer el commit ${commit_2_master} de la rama master`;
                 error_critical = this.msg_err;
                 should.not.exist(error_critical);
             }
@@ -131,35 +130,34 @@ describe('mooc_git-entrega1_merge', function () {
         }
     });
 
-    it("", async function () {
+    it("Buscando x^4 en los commits de la rama master", async function () {
         const expected = "x^4";
-        this.name = `5. Looking for 'x^4' in master branch second commit contents`;
-        this.score = 1;
+        this.score = 0.25;
         if (error_critical) {
             this.msg_err = error_critical;
             should.not.exist(error_critical);
         } else {
             let output;
-            this.msg_ok = `Found '${expected}' in master branch commit ${commit_2_master}`;
-            this.msg_err = `'${expected}' not found in master branch commit ${commit_2_master}`;
+            this.msg_ok = `Se ha encontrado '${expected}' en la rama master ${commit_2_master}`;
+            this.msg_err = `No se ha encontrado'${expected}' en la rama master ${commit_2_master}`;
             [err_show, output] = await to(mygit.show([commit_2_master]));
             Utils.search(expected, output).should.be.equal(true);
         }
     });
+    ////////////////////////////////////////////////////////////////////////////////////////
 
-    it("", async function () {
+    it("Buscando la rama 'sine'", async function () {
         const expected = BRANCH_NAME;
-        this.name = `6. Looking for branch '${BRANCH_NAME}'`;
-        this.score = 2;
+        this.score = 3;
         if (error_critical) {
             this.msg_err = error_critical;
             should.not.exist(error_critical);
         } else {
             let output;
-            this.msg_ok = `Found branch '${BRANCH_NAME}'`;
+            this.msg_ok = `Encontrada la rama 'sine'`;
             [error_branch, branches] = await to(mygit.branch());
             if (error_branch) {
-                this.msg_err = `Error reading branches from ${PATH_REPO}`;
+                this.msg_err = `Error al leer las ramas ${PATH_REPO}`;
                 error_critical = this.msg_err;
                 should.not.exist(error_critical);
             } else {
@@ -167,7 +165,7 @@ describe('mooc_git-entrega1_merge', function () {
             }
             const no_branch = !Utils.search(expected, output);
             if (no_branch){
-                this.msg_err = `Branch '${BRANCH_NAME}' not found`;
+                this.msg_err = `La rama 'sine' no se ha encontado`;
                 error_critical = this.msg_err;
                 should.not.exist(error_critical);
             }
@@ -175,33 +173,31 @@ describe('mooc_git-entrega1_merge', function () {
         }
     });
 
-    it("", async function () {
-        this.name = `7. Checking that the branch '${BRANCH_NAME}' is integrated with master`;
-        this.score = 1;
+    it("Comprobando que se ha integrado la rama 'sine' en la rama master", async function () {
+        this.score = 3;
         if (error_critical) {
             this.msg_err = error_critical;
             should.not.exist(error_critical);
         } else {
             const expected = branches.branches["master"].commit;
             const output = branches.branches[BRANCH_NAME].commit;
-            this.msg_ok = `The branch '${BRANCH_NAME}' is integrated with master`;
-            this.msg_err = `The last commit ${output} from branch '${BRANCH_NAME}' is not integrated with master commit ${expected}`;
+            this.msg_ok = `La rama 'sine' se ha integrado en master`;
+            this.msg_err = `El último commit ${output} de la rama 'sine' no se ha integrado con el commit ${expected} de la rama master`;
             commit_head_sine = output;
             Utils.search(expected, output).should.be.equal(true);
         }
     });
 
-    it("", async function () {
+    it("Buscando 'sin' en el contenido integrado en la rama master", async function () {
         const expected = "sin";
-        this.name = `8. Looking for '${expected}' in final master merge contents`;
-        this.score = 2;
+        this.score = 3;
         if (error_critical) {
             this.msg_err = error_critical;
             should.not.exist(error_critical);
         } else {
             let output;
-            this.msg_ok = `Found '${expected}' in master merge commit ${commit_head_sine} contents`;
-            this.msg_err = `'${expected}' not found in final master merge commit ${commit_head_sine} contents`;
+            this.msg_ok = `Se ha encontrado '${expected}' en la rama master ${commit_head_sine}`;
+            this.msg_err = `No se ha encontrado '${expected}' en la rama master ${commit_head_sine}`;
             [err_show, output] = await to(mygit.show([commit_head_sine]));
             Utils.search(expected, output).should.be.equal(true);
         }
